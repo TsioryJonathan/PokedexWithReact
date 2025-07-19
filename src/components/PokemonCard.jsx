b483fbc3cb134a26eb4c6dba045a3ca7e643daf8
Ania-su <antsaaa.randria@gmail.com>
2025-07-19 09:10:52 +0300

    style: change card style

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -19,7 +19,7 @@ function PokemonCard({ pokemonName }) {
   return (
     <Link
       key={pokemon.id}
-      className={`relative to-white rounded-xl shadow-md p-5 text-center border-gray-300 h-fit pb-10 group transition-transform duration-300 ease-in-out ${getTypeColor(
+      className={`group relative to-white rounded-xl shadow-md border-t-3 border-l-3 border-b-3 border-r-3 border-t-gray-400 border-r-gray-500 border-b-gray-700 p-5 text-center border-gray-300 h-fit pb-10 hover:border-yellow-800 hover:scale-96 transition-transform duration-300 ease-in-out bg-gradient-to-t to-white/70 ${getTypeColor(
         pokemon.types[0].toLowerCase()
       )} `}
       to={`pokemon/${pokemon.name}`}
@@ -30,26 +30,26 @@ function PokemonCard({ pokemonName }) {
         <img
           src="/src/assets/BG.png"
           alt=""
-          className="absolute inset-0 w-full h-full object-cover opacity-30 brightness-75 "
+          className="absolute inset-0 w-full h-full object-cover opacity-30 brightness-75 border-t-3 border-l-3 border-b-3 border-r-3 border-t-gray-400 border-r-gray-500 border-b-gray-700"
         />
         {/* Pokemon image */}
         <img
           src={pokemon.image}
           alt={pokemon.name}
-          className="relative z-10 max-h-full object-contain group-hover:scale-105 transition-all ease-in-out duration-150"
+          className="relative z-10 max-h-full object-contain group-hover:scale-110 transition-all ease-in-out duration-300"
         />
       </div>
 
       {/* Text Info */}
       <h1 className="text-2xl font-extrabold mt-4 capitalize text-gray-900">
-        {pokemon.name}
+        -{pokemon.name}-
       </h1>
-      <h2 className="text-sm text-gray-600 mb-3">#{pokemon.id}</h2>
+      <h2 className="text-sm text-gray-600 mb-3 mt-2">#{pokemon.id}</h2>
 
       {/* Types */}
       <div className="flex justify-center gap-2 mt-2 text-sm font-medium">
         {pokemon.types.map((t) => (
-          <div className="bg-gray-200/50 px-2 py-1 rounded-md flex gap-2 items-center">
+          <div className="bg-gray-200/50 px-2 py-1 rounded-md flex gap-2 items-center border-t-3 border-l-3 border-b-3 border-r-3 border-t-gray-400 border-r-gray-500 border-b-gray-700">
             <span>{getTypeEmoji(t)}</span>
             {t}
           </div>

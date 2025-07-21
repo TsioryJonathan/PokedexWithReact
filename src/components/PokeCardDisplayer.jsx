d344629cd3a543c96d1516bdf4fb896a284e85e9
Mathieu <hei.tafita.2@gmail.com>
2025-07-21 11:26:08 +0300

    fix: auto useEffect to cancel autoscroll if firstRender

--- src/components/PokeCardDisplayer.jsx
+++ src/components/PokeCardDisplayer.jsx
@@ -6,8 +6,13 @@ const PokeCardDisplayer = ({ page, pokemonList, itemsPerPage }) => {
   const endIndex = startIndex + itemsPerPage;
   const toShow = pokemonList.slice(startIndex, endIndex);
   const containerRef = useRef(null);
-
+  const isFirstRender = useRef(true);
+  
   useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
     if (containerRef.current) {
       containerRef.current.scrollIntoView({
         block: "start",

01b92b9a563bff98341d011979001e25fe5b10ab
Mathieu <hei.tafita.2@gmail.com>
2025-07-21 19:52:24 +0300

    feat: add FavoriteProvider and favoriteButton

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import React, { lazy, Suspense } from "react";
 import FullScreenLoader from "./components/Loader/FullScreenLoader";
 import { CompareButton } from "./components/compare/CompareButton";
+import { FavoritesProvider } from "./contexts/FavoritesContext";
+import FavoriteModalButton from "./components/favorite/FavoriteModalButton";
 
 const HeroSection = lazy(() => import("./components/HeroSection"));
 const Home = lazy(() => import("./components/Home"));
 
 function App() {
   return (
-    <Suspense fallback={<FullScreenLoader />}>
-      <HeroSection />
-      <Home />
-      <CompareButton />
-    </Suspense>
+    <FavoritesProvider>
+      <Suspense fallback={<FullScreenLoader />}>
+        <HeroSection />
+        <Home />
+        <CompareButton />
+        <FavoriteModalButton />
+      </Suspense>
+    </FavoritesProvider>
   );
 }
 

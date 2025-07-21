305ecde0f3eda2173c0dced22b05eb63c2f9d222
Mathieu <hei.tafita.2@gmail.com>
2025-07-21 20:03:31 +0300

    refactor: use ModalBase component and change some text

--- src/components/compare/MiniCardSkeleton.jsx
+++ src/components/compare/MiniCardSkeleton.jsx
@@ -5,10 +5,10 @@ export const MiniCardSkeleton = ({className}) => {
     <div className={`rounded-lg shadow p-4 text-center overflow-hidden ${className}`}>
       <div className="flex gap-4">
         <div className="mt-4 w-[40%] flex flex-col items-center justify-center">
-          <Skeleton className="h-6 w-full mx-auto mt-4 rounded-md bg-gray-300" />
-          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-300" />
-          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-300" />
-          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-300" />
+          <Skeleton className="h-6 w-full mx-auto mt-4 rounded-md bg-gray-200" />
+          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-200" />
+          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-200" />
+          <Skeleton className="h-4 w-full mx-auto mt-2 rounded-md bg-gray-200" />
           <div className="flex justify-center gap-2 mt-4">
             <Skeleton className="h-6 w-16 rounded-xl bg-gray-300" />
             <Skeleton className="h-6 w-16 rounded-xl bg-gray-300" />
